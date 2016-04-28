f07125ab7bd43e1b4af58cee6122c916f70e2e02
madmann8 <madmann686@gmail.com>
2016-04-27 21:40:17 -0400

    FIrst commit

--- sequence/fib/fib.go
+++ sequence/fib/fib.go
@@ -1,19 +1,78 @@
 //Needs work still
 package fib
 
-import (
-	"fmt"
-)
+import()
 
-func Fib (n int) int {
+type Fib struct{
+	n int
+	m int
+}
+
+func  (fib *Fib) Fib() {
 	p:= 0
 	q:= 1
-	for i := 0; i < n; i++ {
+	for i := 0; i < fib.n; i++ {
 		p, q = q, p+q
 	}
 	return p
 }
 
-func add
+func (fib *Fib) Add() {
+	p:= 0
+	q:= 1
+	for i := 0; i < fib.n; i++ {
+		p, q = q, p+q
+	}
+	return p+fib.m
+}
+
+func (fib *Fib) Mul() {
+	p:= 0
+	q:= 1
+	for i := 0; i < fib.n; i++ {
+		p, q = q, p+q
+	}
+	return p*fib.m
+}
+
+func (fib *Fib) Div() {
+	p:= 0
+	q:= 1
+	for i := 0; i < fib.n; i++ {
+		p, q = q, p+q
+	}
+	return p/fib.m
+}
+
+func (fib *Fib) Sum() {
+	p:= 0
+	q:= 1
+	s:=0
+	for i := 0; i < fib.n; i++ {
+		p, q = q, p+q
+		s+=p
+	}
+	return s
+}
+
+func (fib *Fib) Phi() {
+	p:= 0
+	q:= 1
+	for i := 0; i < fib.n-1; i++ {
+		p, q = q, p+q
+	}
+	r:=p
+	p= 0
+	q= 1
+	for i := 0; i < fib.n; i++ {
+		p, q = q, p+q
+	}
+	g:=p/r
+	return g
+}
+
+
+
+
 
 

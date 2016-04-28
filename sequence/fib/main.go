ef6d4706ec2c74ef5200283ab06a10846d420abc
madmann8 <madmann686@gmail.com>
2016-04-27 21:14:04 -0400

    FIrst commit

--- sequence/fib/main.go
+++ sequence/fib/main.go
@@ -1,14 +1,14 @@
+//Needs work still
 package main
 
 import (
 	"fmt"
 )
-//Needs work still
 func main()  {
 	fmt.Println(Fib(378378733))
 }
 
-
+//Just basic program, still need to add methods etc.
 func Fib (n int) int {
 	p:= 0
 	q:= 1

91055067edcbaba614bd169049575c04e9a5890b
madmann8 <madmann686@gmail.com>
2016-05-29 23:03:32 -0400

    No errors but values incorrect.. needs work

--- sequence/fib/fib.go
+++ sequence/fib/fib.go
@@ -1,88 +1,84 @@
 // Package fib allows for usage of various Fibonacci Numbers
 
-package fib
+package main
 
 import ()
 import (
 	"math"
 )
 
-//Specifies the corresponding Fibonacci Number for the N value inputted
+
 type Fib struct {
 	FibNum float64
-	Phi float64
+	Phi    float64
 }
 
-func NewSeq(s string) *Fib {
-	var s = &Fib{
+func NewFib() *Fib {
+	var f = &Fib{
 		FibNum: 0,
-		Phi: 0,
+		Phi:    1.6,
 	}
+	f.Phi = (1 + math.Sqrt(5)) / 2
+	return f
 
-	return s
 }
 
-func (Fib *Fib) Fib(N float64) *Fib  {
-	Fib.FibNum=(Fib.Phi^(N)-(-1*Fib.Phi)^(-1*N))/math.Sqrt(5)
-	return Fib
+func (f *Fib) FibN(N float64) *Fib {
+	f.FibNum = ((math.Pow(f.Phi, N)) - math.Pow((-1*f.Phi), (-1*N))/math.Sqrt(5))
+	return f
 }
 
 //Adds the N and M Fibonacci Values
 
-func (Fib *Fib) Add (N float64) *Fib {
-	r:= 0
-	t:= 1
-	for i := 0; i < N; i++ {
+func (f *Fib) Add(N float64) *Fib {
+	r := 0
+	t := 1
+	for i := 0; float64(i) < N; i++ {
 		r, t = t, r+t
 	}
-	Fib.FibNum+=r
-	return Fib
+	f.FibNum += float64(r)
+	return f
 }
 
 //Multiplies the N and M Fibonacci Values
 
-func  (Fib *Fib) Mul(M float64) *Fib {
-	
-	r:= 0
-	t:= 1
-	for i := 0; i < M; i++ {
+func (f *Fib) Mul(M float64) *Fib {
+
+	r := 0
+	t := 1
+	for i := 0; float64(i) < M; i++ {
 		r, t = t, r+t
 	}
-	Fib.FibNum*=r
-	return Fib
+	f.FibNum *= float64(r)
+	return f
 }
 
 //Divides the N and M Fibonacci Values
 
-func (Fib *Fib) Div(N float64) *Fib {
+func (f *Fib) Div(N float64) *Fib {
 	var r float64
 	var t float64
-	r= 0
-	t= 1
+	r = 0
+	t = 1
 	for i := 0; float64(i) < float64(N); i++ {
 		r, t = t, r+t
 	}
-	Fib.FibNum/=r
-	return Fib
+	f.FibNum /= r
+	return f
 
 }
 
 //Adds all Fibonacci values prior to N and N
 
-func (Fib *Fib) Sum(N float64) *Fib {
-	p:= 0
-	q:= 1
-	s:=0
-	for i := 0; i < N; i++ {
+func (f *Fib) Sum(N float64) *Fib {
+	p := 0
+	q := 1
+	s := 0
+	for i := 0; float64(i) < float64(N); i++ {
 		p, q = q, p+q
-		s+=p
+		s += p
 	}
-	return Fib
+	return f
 }
 
 //Calculates the Golden Ratio using N and M
-
-func (Fib *Fib) Phi() *Fib {
-	Fib.Phi=(1+math.Sqrt(5))/2
-	return Fib
-}

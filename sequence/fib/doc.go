fdde3f56ee2dc10217327983e0c34cab31658585
madmann8 <madmann686@gmail.com>
2016-05-30 19:33:47 -0400

    Final Code

--- sequence/fib/doc.go
+++ sequence/fib/doc.go
@@ -1,1 +1,3 @@
 //This package preforms various tasks in regards to the Fibinacci sequence. These include adding, multiplying, and dividing fibonacci numbers. Also it can calculate the golden ration using Phi.
+//NewFib is required prior to FibN,Add,Mul,and Div
+//End is required after FibN,Add,Mul,and Div

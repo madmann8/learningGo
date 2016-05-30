02f17e58e99222e4f154423879bd84f8d6817295
madmann8 <madmann686@gmail.com>
2016-05-30 19:27:54 -0400

    Create doc.go

---
+++ sequence/fib/doc.go
@@ -0,0 +1,1 @@
+//This package preforms various tasks in regards to the Fibinacci sequence. These include adding, multiplying, and dividing fibonacci numbers. Also it can calculate the golden ration using Phi.
